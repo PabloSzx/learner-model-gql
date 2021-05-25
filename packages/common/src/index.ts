3e3787a759c44ea530a20b291d35516981bf42ea
PabloSzx <pablosaez1995@gmail.com>
2021-05-25 10:27:55 -0400

    change default client api url

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -12,7 +12,8 @@ export const ENV = {
 export const IS_CI = !!process.env.CI;
 export const IS_NOT_CI = !IS_CI;
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/graphql";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://learner-model.pablosz.tech/graphql";
 
 export const Auth0Verify = fp(async (app) => {
   const { AUTH0_DOMAIN, AUTH0_CLIENT, AUTH0_SECRET } = process.env;
