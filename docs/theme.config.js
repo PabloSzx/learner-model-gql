f51c80ed5fd22cfd6929e9fdc6d35323fee48e95
PabloSzx <pablosaez1995@gmail.com>
2021-09-04 15:21:00 -0400

    add docs package

---
+++ docs/theme.config.js
@@ -0,0 +1,12 @@
+export default {
+  github: "https://github.com/PabloSzx/learner-model-gql",
+  docsRepositoryBase:
+    "https://github.com/PabloSzx/learner-model-gql/blob/main/docs/",
+
+  titleSuffix: " - Learner Model GQL",
+  head: <></>,
+  logo: <>Learner Model GQL</>,
+  prevLinks: true,
+  nextLinks: true,
+  footer: true,
+};
