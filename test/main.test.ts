d5ca586a4c85c1256a22a7dabe44acc4e9701f80
PabloSzx <pablosaez1995@gmail.com>
2021-09-08 20:54:54 -0300

    gateway tests add content service

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,30 +1,54 @@
-import { CheckActionsCreationRetrieval } from "../packages/services/actions/test/main.test";
 import { getStitchedSchema } from "../packages/gateway/src/stitch";
 import { actionModule } from "../packages/services/actions/src/modules";
+import { CheckActionsCreationRetrieval } from "../packages/services/actions/test/main.test";
+import {
+  contentModule,
+  domainModule,
+} from "../packages/services/content/src/modules";
+import { CheckContentCreationRetrieval } from "../packages/services/content/test/main.test";
 import {
   expectDeepEqual,
   GetTestClient,
   HelloDocument,
 } from "../packages/testing/src/index";
 
+export const TestStitchedSchema = async () => {
+  const ActionService = await GetTestClient({
+    prepare({ registerModule }) {
+      registerModule(actionModule);
+    },
+  });
+
+  const ContentService = await GetTestClient({
+    prepare({ registerModule }) {
+      registerModule(contentModule);
+      registerModule(domainModule);
+    },
+  });
+
+  const stitchedSchema = await getStitchedSchema([
+    {
+      name: "actions",
+      href: ActionService.origin,
+    },
+    {
+      name: "content",
+      href: ContentService.origin,
+    },
+  ]);
+
+  const GatewayClient = await GetTestClient({
+    schema: stitchedSchema,
+  });
+
+  return {
+    GatewayClient,
+  };
+};
+
 describe("gateway", () => {
   it("Gateway Hello World", async () => {
-    const ActionService = await GetTestClient({
-      prepare({ registerModule }) {
-        registerModule(actionModule);
-      },
-    });
-
-    const stitchedSchema = await getStitchedSchema([
-      {
-        name: "actions",
-        href: ActionService.origin,
-      },
-    ]);
-
-    const GatewayClient = await GetTestClient({
-      schema: stitchedSchema,
-    });
+    const { GatewayClient } = await TestStitchedSchema();
 
     expectDeepEqual(await GatewayClient.query(HelloDocument), {
       data: {
@@ -34,23 +58,14 @@ describe("gateway", () => {
   });
 
   it("actions", async () => {
-    const ActionService = await GetTestClient({
-      prepare({ registerModule }) {
-        registerModule(actionModule);
-      },
-    });
-
-    const stitchedSchema = await getStitchedSchema([
-      {
-        name: "actions",
-        href: ActionService.origin,
-      },
-    ]);
-
-    const GatewayClient = await GetTestClient({
-      schema: stitchedSchema,
-    });
+    const { GatewayClient } = await TestStitchedSchema();
 
     await CheckActionsCreationRetrieval(GatewayClient);
+  });
+
+  it("content", async () => {
+    const { GatewayClient } = await TestStitchedSchema();
+
+    await CheckContentCreationRetrieval(GatewayClient);
   });
 });
