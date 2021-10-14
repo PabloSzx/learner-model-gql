92626a21db498534601108298759233c4d73a921
PabloSzx <pablosaez1995@gmail.com>
2021-10-14 13:09:30 -0300

    update pino-pretty usage

--- packages/api-base/src/logger.ts
+++ packages/api-base/src/logger.ts
@@ -4,9 +4,13 @@ import { ENV } from "common";
 
 export const logger = Pino({
   level: ENV.IS_DEVELOPMENT ? "info" : "info",
-  prettyPrint: {
-    levelFirst: true,
-    singleLine: true,
-    translateTime: true,
+  //@ts-expect-error
+  transport: {
+    target: "pino-pretty",
+    options: {
+      levelFirst: true,
+      singleLine: true,
+      translateTime: true,
+    },
   },
 });
