8973a0004629fd9a100b2b85aece1bd2d23f6ae6
PabloSzx <pablosaez1995@gmail.com>
2021-10-15 12:48:00 -0300

    update pino

--- packages/api-base/src/logger.ts
+++ packages/api-base/src/logger.ts
@@ -4,7 +4,6 @@ import { ENV } from "common";
 
 export const logger = Pino({
   level: ENV.IS_DEVELOPMENT ? "info" : "info",
-  //@ts-expect-error
   transport: {
     target: "pino-pretty",
     options: {
