c70a33bff3a8e0c0d5f740fbe946d00bfa7d6eff
Pablo Sáez <pablosaez1995@gmail.com>
2021-10-16 15:13:58 -0300

    DB_PREPARED promise

--- packages/api-base/src/listen.ts
+++ packages/api-base/src/listen.ts
@@ -1,12 +1,16 @@
 import type { FastifyInstance } from "fastify";
+import { DB_PREPARED } from "db";
 import waitOn from "wait-on";
 
 export async function smartListen(app: FastifyInstance, port: number) {
-  await waitOn({
-    reverse: true,
-    resources: ["tcp:" + port],
-    timeout: 5000,
-  });
+  await Promise.all([
+    waitOn({
+      reverse: true,
+      resources: ["tcp:" + port],
+      timeout: 5000,
+    }),
+    DB_PREPARED,
+  ]);
 
   await app.listen(port, "0.0.0.0");
 }
--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,6 +1,6 @@
-import { ENV } from "common-api";
-
+import { LazyPromise } from "@graphql-ez/utils/promise";
 import PrismaClient from "@prisma/client";
+import { ENV } from "common-api";
 
 export const prisma = new PrismaClient.PrismaClient({
   log: ENV.IS_DEVELOPMENT
@@ -8,6 +8,22 @@ export const prisma = new PrismaClient.PrismaClient({
     : ENV.IS_TEST
     ? ["error", "warn"]
     : ["info", "error", "warn"],
+});
+
+export const DB_PREPARED = LazyPromise(async () => {
+  await prisma.user.upsert({
+    create: {
+      email: "pablosaez1995@gmail.com",
+      role: "ADMIN",
+    },
+    update: {
+      role: "ADMIN",
+    },
+    where: {
+      email: "pablosaez1995@gmail.com",
+    },
+    select: null,
+  });
 });
 
 export * from "@prisma/client";
