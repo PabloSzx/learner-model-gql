c5f4409e1491eb827f6a1bf8589bf3e65e0f8711
PabloSzx <pablosaez1995@gmail.com>
2021-11-09 18:08:45 -0300

    db ModelState

---
+++ packages/db/prisma/migrations/20211109210758_add_model_state/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE "ModelStateType" (
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ModelStateType_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "ModelStateCreator" (
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ModelStateCreator_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "ModelState" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "type" TEXT,
+    "creator" TEXT NOT NULL,
+    "domainId" INTEGER NOT NULL,
+    "json" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ModelState_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ModelStateType_name_key" ON "ModelStateType"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ModelStateCreator_name_key" ON "ModelStateCreator"("name");
+
+-- AddForeignKey
+ALTER TABLE "ModelState" ADD CONSTRAINT "ModelState_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ModelState" ADD CONSTRAINT "ModelState_type_fkey" FOREIGN KEY ("type") REFERENCES "ModelStateType"("name") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ModelState" ADD CONSTRAINT "ModelState_creator_fkey" FOREIGN KEY ("creator") REFERENCES "ModelStateCreator"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ModelState" ADD CONSTRAINT "ModelState_domainId_fkey" FOREIGN KEY ("domainId") REFERENCES "Domain"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
