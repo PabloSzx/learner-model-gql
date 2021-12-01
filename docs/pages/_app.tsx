95647ad0e1b17e159a4a8c0a8e85703c190badca
PabloSzx <pablosaez1995@gmail.com>
2021-12-01 01:33:43 -0300

    auth docs progress

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,5 @@
-import "nextra-theme-docs/style.css";
-
 import type { AppProps } from "next/app";
+import "nextra-theme-docs/style.css";
 
 export default function Nextra({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- docs/theme.config.js
+++ docs/theme.config.js
@@ -9,4 +9,5 @@ export default {
   prevLinks: true,
   nextLinks: true,
   footer: true,
+  search: true,
 };
