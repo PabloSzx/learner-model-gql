d753077e6b341d78ba7e1356a7df6a690b794250
PabloSzx <pablosaez1995@gmail.com>
2021-12-03 00:05:40 -0300

    respect PORT env variable

--- packages/api-base/src/listen.ts
+++ packages/api-base/src/listen.ts
@@ -3,14 +3,16 @@ import { DB_PREPARED } from "db";
 import waitOn from "wait-on";
 
 export async function smartListen(app: FastifyInstance, port: number) {
+  const listenPort = process.env.PORT ? parseInt(process.env.PORT) : port;
+
   await Promise.all([
     waitOn({
       reverse: true,
-      resources: ["tcp:" + port],
+      resources: ["tcp:" + listenPort],
       timeout: 5000,
     }),
     DB_PREPARED,
   ]);
 
-  await app.listen(port, "0.0.0.0");
+  await app.listen(listenPort, "0.0.0.0");
 }
