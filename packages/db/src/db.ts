63c30e986400a412434c41fde3c6150d0d8d85c5
PabloSzx <pablosaez1995@gmail.com>
2021-12-03 23:26:06 -0300

    change admin user upsert to use env variable

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,6 +1,8 @@
 import { LazyPromise } from "@graphql-ez/utils/promise";
 import PrismaClient from "@prisma/client";
-import { ENV } from "common-api";
+import assert from "assert/strict";
+import { ENV, logger } from "common-api";
+import isEmail from "validator/lib/isEmail.js";
 
 export const prisma = new PrismaClient.PrismaClient({
   log: ENV.IS_DEVELOPMENT
@@ -10,24 +12,36 @@ export const prisma = new PrismaClient.PrismaClient({
     : ["info", "error", "warn"],
 });
 
+const ADMIN_USER_EMAIL = process.env.ADMIN_USER_EMAIL;
+
+if (ADMIN_USER_EMAIL) {
+  assert(
+    isEmail(ADMIN_USER_EMAIL),
+    `Invalid Email set as ADMIN_USER_EMAIL: "${ADMIN_USER_EMAIL}"`
+  );
+}
+
 export const DB_PREPARED = LazyPromise(async () => {
-  await prisma.user.upsert({
-    create: {
-      email: "pablosaez1995@gmail.com",
-      role: "ADMIN",
-    },
-    update: {
-      role: "ADMIN",
-    },
-    where: {
-      email: "pablosaez1995@gmail.com",
-    },
-    select: null,
-  });
+  if (ADMIN_USER_EMAIL) {
+    await prisma.user.upsert({
+      create: {
+        email: ADMIN_USER_EMAIL,
+        role: "ADMIN",
+      },
+      update: {
+        role: "ADMIN",
+      },
+      where: {
+        email: ADMIN_USER_EMAIL,
+      },
+      select: null,
+    });
+
+    logger.warn(`EMAIL "${ADMIN_USER_EMAIL}" HAS BEEN SET AS ADMIN USER`);
+  }
 });
 
 export const Prisma = PrismaClient.Prisma;
 
-export type { Prisma as PrismaNS } from "@prisma/client";
-
 export * from "@prisma/client";
+export type { Prisma as PrismaNS } from "@prisma/client";
