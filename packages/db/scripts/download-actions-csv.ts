cae797b04197682fa6f41788508066d0893b5ac4
PabloSzx <pablosaez1995@gmail.com>
2021-12-09 18:57:49 -0300

    database tooling docs

--- packages/db/scripts/download-actions-csv.ts
+++ packages/db/scripts/download-actions-csv.ts
@@ -2,7 +2,22 @@ import { writeFile } from "fs/promises";
 import json2csv from "json2csv";
 import { prisma } from "../src";
 
-const actions = await prisma.action.findMany();
+const actions = await prisma.action.findMany({
+  include: {
+    user: {
+      select: {
+        email: true,
+        name: true,
+      },
+    },
+    project: {
+      select: {
+        code: true,
+        label: true,
+      },
+    },
+  },
+});
 
 const now = new Date().toISOString();
 
--- packages/db/scripts/download-actions-json.ts
+++ packages/db/scripts/download-actions-json.ts
@@ -1,7 +1,22 @@
 import { writeFile } from "fs/promises";
 import { prisma } from "../src";
 
-const actions = await prisma.action.findMany();
+const actions = await prisma.action.findMany({
+  include: {
+    user: {
+      select: {
+        email: true,
+        name: true,
+      },
+    },
+    project: {
+      select: {
+        code: true,
+        label: true,
+      },
+    },
+  },
+});
 
 const now = new Date().toISOString();
 
