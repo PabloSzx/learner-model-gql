67d1eb38161e9ae9afa71f41e70b513c4aa1a740
PabloSzx <pablosaez1995@gmail.com>
2022-01-23 20:42:42 -0300

    don't console.log on browser

--- packages/common/src/env.ts
+++ packages/common/src/env.ts
@@ -13,7 +13,8 @@ export const IS_PRODUCTION = NODE_ENV === "production";
 export const IS_DEVELOPMENT = NODE_ENV === "development";
 export const IS_TEST = NODE_ENV === "test";
 
-!process.env.SILENT &&
+typeof window === "undefined" &&
+  !process.env.SILENT &&
   console.log({
     IS_PRODUCTION,
     IS_DEVELOPMENT,
