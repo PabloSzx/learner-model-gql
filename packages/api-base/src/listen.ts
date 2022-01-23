f06e32ef0fda49ef7226668403438e65fdc3a4ad
PabloSzx <pablosaez1995@gmail.com>
2022-01-23 19:40:46 -0300

    remove wait on port in smartListen

--- packages/api-base/src/listen.ts
+++ packages/api-base/src/listen.ts
@@ -1,18 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import { DB_PREPARED } from "db";
-import waitOn from "wait-on";
 
 export async function smartListen(app: FastifyInstance, port: number) {
   const listenPort = process.env.PORT ? parseInt(process.env.PORT) : port;
 
-  await Promise.all([
-    waitOn({
-      reverse: true,
-      resources: ["tcp:" + listenPort],
-      timeout: 5000,
-    }),
-    DB_PREPARED,
-  ]);
+  await Promise.all([DB_PREPARED]);
 
   await app.listen(listenPort, "0.0.0.0");
 }
