4f38b9cf31903991968a6f3a6c326296c79546b3
PabloSz <pablosaez1995@gmail.com>
2022-07-04 16:54:58 -0400

    use wait-on on development listen

--- packages/api-base/src/listen.ts
+++ packages/api-base/src/listen.ts
@@ -1,10 +1,20 @@
 import type { FastifyInstance } from "fastify";
 import { DB_PREPARED } from "db";
+import waitOn from "wait-on";
+import { IS_DEVELOPMENT } from "common-api";
 
 export async function smartListen(app: FastifyInstance, port: number) {
   const listenPort = process.env.PORT ? parseInt(process.env.PORT) : port;
 
   await Promise.all([DB_PREPARED]);
+
+  if (IS_DEVELOPMENT) {
+    await waitOn({
+      resources: ["tcp:" + listenPort],
+      reverse: true,
+      timeout: 5000,
+    });
+  }
 
   await app.listen({
     port: listenPort,
